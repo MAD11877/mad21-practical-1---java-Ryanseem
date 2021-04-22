5e84e54329c3163da8b399b161b6c95ea1b21787
Ryan Seem <>
2021-04-22 07:06:16 +0000

    final

--- Question3.java
+++ Question3.java
@@ -11,7 +11,7 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter an integer: ");
+    //System.out.print("Enter an integer: ");
     int input = in.nextInt();
     int result =  input * input;
     System.out.println(result);
